cc1859a5aabe982bd7dfeec91107b58abf3f1e6a
Cody Coljee-Gray <cody.cg@gmail.com>
2025-06-16 21:36:25 -0700

    chore: format vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,29 @@ import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
   test: {
-    exclude: ['node_modules', 'dist', 'build', 'public', 'src/assets', '*.config.*', 'e2e', '.astro'],
+    exclude: [
+      'node_modules',
+      'dist',
+      'build',
+      'public',
+      'src/assets',
+      '*.config.*',
+      'e2e',
+      '.astro',
+    ],
     coverage: {
-      exclude: ['node_modules', 'dist', 'build', 'public', 'src/assets', '*.config.*', 'e2e', '.astro'],
+      exclude: [
+        'node_modules',
+        'dist',
+        'build',
+        'public',
+        'src/assets',
+        '*.config.*',
+        'e2e',
+        '.astro',
+      ],
     },
     environment: 'happy-dom',
     globals: true,
   },
-}); 
+});
