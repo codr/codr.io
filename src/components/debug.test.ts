535d1096fd743754ffed39f2ce532ea4df47ebaa
Cody Coljee-Gray <68203+codr@users.noreply.github.com>
2026-08-02 12:22:15 -0700

    chore: remove debug test

--- src/components/debug.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest';
-import Welcome from './Welcome.astro';
-
-describe('debug', () => {
-  it('checks function source', () => {
-    console.log(Welcome.toString().slice(0, 200));
-  });
-});
