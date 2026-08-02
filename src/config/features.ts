77ce4458be36afe43593cc969e5b21b4d202286e
Cody Coljee-Gray <68203+codr@users.noreply.github.com>
2026-08-02 11:56:43 -0700

    feat: clean up header

--- src/config/features.ts
+++ src/config/features.ts
@@ -2,4 +2,4 @@ export const FEATURES = {
   SHOW_HEADER_LINKS: false, // Set to true to enable Projects and Blog links
 } as const;
 
-export type Features = typeof FEATURES; 
+export type Features = typeof FEATURES;
